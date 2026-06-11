249d078ceed58166a6374abc61dd539169d6ca56
gop1ka-c <gopikagop724@gmail.com>
2026-06-11 19:01:06 +0530

    Added email automation to Pulse bot

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
 # Pulse - Daily Summary Bot
 
 import requests
+import smtplib
+import os
+from email.message import EmailMessage
 from datetime import date
+import urllib3
 
-CITY="palakkad"
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+CITY = "palakkad"
 
 def get_weather(city=CITY):
     url = f"https://wttr.in/{city}?format=3&m"
@@ -18,13 +24,15 @@ def get_weather(city=CITY):
 
 
 def get_quote():
-    url = "https://zenquotes.io/api/random"
-
     try:
-        response = requests.get(url, timeout=20)
-        response.raise_for_status()
+        response = requests.get(
+            "https://zenquotes.io/api/random",
+            timeout=20,
+            verify=False
+        )
 
         data = response.json()
+
         quote = data[0]["q"]
         author = data[0]["a"]
 
@@ -33,7 +41,24 @@ def get_quote():
     except Exception as e:
         return f"Quote unavailable ({e})"
 
+def send_email(summary):
+    sender = "moaname032@gmail.com"
+    receiver = "moaname032@gmail.com"
 
+    password = os.getenv("EMAIL_PASSWORD")
+
+    msg = EmailMessage()
+    msg["Subject"] = f"Pulse Daily Summary - {date.today()}"
+    msg["From"] = sender
+    msg["To"] = receiver
+
+    msg.set_content(summary)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(sender, password)
+        smtp.send_message(msg)
+
+    print("Email sent successfully!")
 # today = date.today()
 
 # summary = f"""
@@ -49,9 +74,9 @@ def get_quote():
 today = date.today()
 
 summary = f"""
-╔════════════════════════════════════╗
-║         PULSE DAILY SUMMARY        ║
-╚════════════════════════════════════╝
+
+         PULSE DAILY SUMMARY        
+
 
 📅  {today}
 
@@ -68,3 +93,5 @@ with open("daily_summary.txt", "w", encoding="utf-8") as file:
     file.write(summary)
 
 print("Daily summary saved to daily_summary.txt")
+
+send_email(summary)
